b0767b7f5e671deb52071c53c2edee394d6e0d63
vercinclitoris <mixmax313@hotmail.fr>
2017-11-22 17:58:34 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+    <head>
+            <meta charset="utf-8">
+            <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+            <meta name="description" content="Made with WOW Slider - Create beautiful, responsive image sliders in a few clicks. Awesome skins and animations. Carousel jquery" />
+        <title>accueil BianchiDelice</title>
+            <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+            <link rel="stylesheet" type="text/css" href="engine1/style.css" />
+            <link rel="stylesheet" type="text/css" href="css/accueil.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <img src="photo/logoofficiel.png" class="img-responsiv">
+            </nav>
+        </header>
+                <section class="presentation">
+                    <div id="wowslider-container1">
+                        <div class="ws_images">
+                            <ul>
+                                <li>
+                                    <img src="data1/images/famillecomplet.jpg" alt="BianchiDélice une entreprise familile" title="BianchiDélice une entreprise familile" id="wows1_0"/>
+                                </li>
+                                <li>
+                                    <img src="data1/images/fondateur.jpg" alt="image slider" title="Humbert Bianchi" id="wows1_1"/></a>l'entreprise BianchiDelice crée en 1952 par Mr Humbert Bianchi destinée à la livraison de produit surgelés pour le professionnel et le particulier.
+                                </li>
+                                <li>
+                                    <img src="data1/images/catbio1.jpg" alt="Gauthier Bianchi" title="Gauthier Bianchi" id="wows1_2"/>Nouveauté chez BianchiDélice "les produits bio"
+                                    nouveau catalogue "Bio , Blanc , Rouge"
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="ws_bullets">
+                            <div>
+                                <a href="#" title="BianchiDélice une entreprise familile"><span><img src="data1/tooltips/famillecomplet.jpg" id="imgflo" alt="BianchiDélice une entreprise familile"/>1</span></a>
+                                <a href="#" title="Humbert Bianchi"><span><img src="data1/tooltips/fondateur.jpg" id="imgflo" alt="Humbert Bianchi"/>2</span></a>
+                                <a href="#" title="Gauthier Bianchi"><span><img src="data1/tooltips/catbio1.jpg" id="imgflo" alt="Gauthier Bianchi"/>3</span></a>
+                            </div>
+                        </div>
+                        <div class="ws_shadow"></div>
+                    </div>  
+                      <p id="descriptifEntreprise">L'entreprise BianchiDélice crée en 1952 par Mr Humbert Bianchi destinée a la livraison de produits frais, surgelés et bio pour le professionnels et le particuliers.<a href="http://bianchidelice.fr/84-l-entreprise-familiale" class="savoir">en savoir plus</a><br>
+                        BianchiDélice<br>Hameau de Baylac BP/70010/ 32550 Montegut Auch<br><a href="https://login.live.com/">contact@bianchidelice.fr</a><br>05-62-65-68-20<br><a href="https://www.google.fr/maps/dir/''/gascogne+surgeles/@43.6511116,0.5952392,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x12a979ee11332e2b:0x8dd3d8b39282e6bf!2m2!1d0.665279!2d43.651133"><img src="photo/localise.png">Localiser nous</a></p>
+                </section>            
+                    <center>
+                        <div id="carrousel">
+                            <ul>
+                                <a href=""><img src="photo/catbio.jpeg" class="img-responsive"></a>
+                                <a href=""><img src="photo/catclass.jpeg" class="img-responsive"></a>
+                                <a href=""><img src="photo/catmensuel.jpeg" class="img-responsive"></a>
+                            </ul>
+                        </div>
+                    </center>       
+                    <footer class="footer">
+                        <center>
+                        <a href="https://www.facebook.com/search/top/?q=bianchidelice"><img src="photo/facebook.png" id="logoreseaux"></a>
+                        <a href="http://bianchidelice.fr/"><img src="photo/boutique.png" id="logoreseaux"></a>
+                    </center>   
+                    </footer>
+                    
+    
+        
+
+
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/tether.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="engine1/jquery.js"></script>
+        <script type="text/javascript" src="engine1/wowslider.js"></script>
+        <script type="text/javascript" src="engine1/script.js"></script>
+        <script type="text/javascript" src="js/accueil.js"></script>
+    </body>
+</html>
